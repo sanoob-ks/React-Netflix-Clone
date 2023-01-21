c052329cff663839bb9eec9960ed3e8e9333682a
sanoob-ks <119393693+sanoob-ks@users.noreply.github.com>
2023-01-21 09:49:06 +0530

    second commit

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDy4FF0M94GFhuX94p2EvbvbFOtpT3fNwc",
+    apiKey: API_KEY,
     authDomain: "netflix-project-de277.firebaseapp.com",
     projectId: "netflix-project-de277",
     storageBucket: "netflix-project-de277.appspot.com",
-    messagingSenderId: "560192091036",
-    appId: "1:560192091036:web:91193456a495f3c7fbce59",
-    measurementId: "G-QCFR4NW40H"
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+    measurementId:MEASUREMENT_ID
   };
 const Firebase=initializeApp(firebaseConfig);
 const db = getFirestore()
